8533fc19d0fd442cd11ca0d5bfa415ab06cb7f39
Megan <megan.leet@iinet.net.au>
2017-12-27 05:36:43 +1100

    Add 32X, Sega Pico support

---
+++ ROMniscience/Handlers/32X.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMniscience.Handlers {
+	class _32X: Handler {
+		public override IDictionary<string, string> filetypeMap => new Dictionary<string, string>() {
+			{"32x", "Sega 32X ROM"},
+			{"bin", "Sega 32X ROM"},
+		};
+		public override string name => "32X";
+
+		public override void addROMInfo(ROMInfo info, string extension, ROMFile file) {
+			Megadrive.parseMegadriveROM(info, file.stream);
+		}
+	}
+}
---
+++ ROMniscience/Handlers/Pico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMniscience.Handlers {
+	class Pico: Handler {
+		public override IDictionary<string, string> filetypeMap => new Dictionary<string, string>() {
+			//It seems odd to me that Pico uses the .md file extension when that clearly
+			//stands for Megadrive, but eh, I don't make the rules
+			//Sure it's the same ROM format, but so is 32X and that gets its own extension
+			{"md", "Sega Pico ROM"}
+		};
+		public override string name => "Sega Pico";
+
+		public override void addROMInfo(ROMInfo info, string extension, ROMFile file) {
+			Megadrive.parseMegadriveROM(info, file.stream);
+		}
+	}
+}
