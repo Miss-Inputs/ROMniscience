589fe6bcf64484b6480d182652ad88ab6f637d50
Megan Leet <megan.leet@iinet.net.au>
2018-03-06 15:53:56 +1100

    Add extension methods for Stream. I'll refactor all this stuff one day and this is just the beginning

---
+++ ROMniscience/IO/StreamExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMniscience.IO {
+	static class StreamExtensions {
+		public static int read(this Stream s) => s.ReadByte();
+
+		public static byte[] read(this Stream s, int bytes) {
+			byte[] buf = new byte[bytes];
+			int bytesRead = s.Read(buf, 0, bytes);
+			if (bytesRead == 0) {
+				return new byte[] { };
+			} else if (bytesRead == bytes) {
+				return buf;
+			} else {
+				byte[] buf2 = new byte[bytesRead];
+				Array.Copy(buf, buf2, bytesRead);
+				return buf2;
+			}
+		}
+
+		public static String read(this Stream s, int length, Encoding encoding) {
+			return encoding.GetString(s.read(length));
+		}
+
+		public static int readIntBE(this Stream s) {
+			return (s.read() << 24) | (s.read() << 16) | (s.read() << 8) | s.read();
+		}
+
+		public static int readIntLE(this Stream s) {
+			return s.read() | (s.read() << 8) | (s.read() << 16) | (s.read() << 24);
+		}
+
+		public static int readShortBE(this Stream s) {
+			return (s.read() << 8) | s.read();
+		}
+
+		public static int readShortLE(this Stream s) {
+			return s.read() | (s.read() << 8);
+		}
+
+		public static Stream cloneInputStream(Stream s) {
+			return new MemoryStream(s.read((int)s.Length));
+		}
+	}
+}
