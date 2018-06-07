b68795d1757316f35ba4a712121d95a023ae483f
Megan Leet <megan.leet@iinet.net.au>
2018-06-07 17:52:48 +1000

    [VIC-20] Fix up header stuff and file extensions

--- ROMniscience/Handlers/Stubs/Vic20.cs
+++ ROMniscience/Handlers/Stubs/Vic20.cs
@@ -36,12 +36,20 @@ namespace ROMniscience.Handlers.Stubs {
 			{"prg", "Commodore VIC-20 binary executable"},
 			{"c64", "Commodore VIC-20 binary executable"},
 			{"tap", "Commodore VIC-20 raw tape image"},
-			{"p00", "Commodore VIC-20 program file" }
+			{"p00", "Commodore VIC-20 program file"},
+			{"crt", "Commodore VIC-20 ROM"},
+			{"20", "Commodore VIC-20 ROM (load at 0x2000)"},
+			{"40", "Commodore VIC-20 ROM (load at 0x4000)"},
+			{"60", "Commodore VIC-20 ROM (load at 0x6000)"},
+			{"70", "Commodore VIC-20 ROM (load at 0x7000)"},
+			{"a0", "Commodore VIC-20 ROM (load at 0xA000)"},
+			{"b0", "Commodore VIC-20 ROM (load at 0xB000)"}, //Luckily, there's just these 6... or at least according to MAME, anyway
 		};
 
 		public override bool shouldSkipHeader(ROMFile rom) {
-			//Yay, annoyance time! There's .prg files as normal, but then there's also .a0 and .60 and etc files that are all used by No-Intro... well, from what I can tell, these are actually just binary executables except there's normally a 2-byte load address at the beginning, so of course people take that off because headers are evil apparently. How those work is that the load address is determined by the file extension (.a0 = load at 0xa000, .60 = load at 0x6000, .a001 = load at 0xa001 apparently), so that means there's theoretically an infinite number of file extensions and aaaa thanks I hate it
-			return "prg".Equals(rom.extension) && (rom.length % 256) == 2;
+			//Okay kids, so what's happening here is that there's cartridges (crt) and executables (prg), but the thing is they have to be loaded at a specific address. So what happens is that you can have a 2-byte header at the beginning which has the load address in 2-byte little endian, or you can put the load address in the extension and leave the original file untouched (which is what No-Intro does). Hopefully that makes sense because I don't feel like explaining stuff
+			//Although, maybe you don't want to rename a .crt that does have this header to something like .a0, which you might do if you skip this header and have a headered cartridge identified as something which doesn't... but ehhhhh. When I make this a not stub handler, I should just detect the presence of the header and whatnot, and that should make things nice and user-friendly
+			return ("prg".Equals(rom.extension) || "crt".Equals(rom.extension)) && (rom.length % 256) == 2;
 		}
 
 		public override int skipHeaderBytes() {
