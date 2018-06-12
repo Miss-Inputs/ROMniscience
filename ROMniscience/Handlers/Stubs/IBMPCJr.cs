ae07f797fac77fa47307e78454da2c331c59f9e7
Megan Leet <megan.leet@iinet.net.au>
2018-06-12 23:25:08 +1000

    [Stub handlers] Add IBM PC Jr stub handler
    Those carts are basically "PC booter" things, so they wouldn't be suitable for any kind of DOS handler later. Would I end up making a PC booter handler? Does that make sense? I might have to find out how PC booter floppies work, because that sounds mildly fun

---
+++ ROMniscience/Handlers/Stubs/IBMPCJr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMniscience.Handlers.Stubs {
+	class IBMPCJr : StubHandler {
+		public override IDictionary<string, string> filetypeMap => new Dictionary<string, string> {
+			{"bin", "IBM PC Jr. ROM"},
+			{"jrc", "IBM PC Jr. ROM"}, //There might be a difference here, actually... TOSEC just uses .jrc universally, and nobody else has bothered to make any kind of "authorative" datfile of PCjr carts
+			//TODO Floppies
+		};
+
+		public override string name => "IBM PC Jr.";
+	}
+}
