3edbf1da5995fce103cc11b89246eab219ddd044
Megan Leet <megan.leet@iinet.net.au>
2018-07-07 01:41:07 +1000

    Added a whole bunch of stub handlers for computer systems.
    I'm not gonna say that's all of them, because I think I said that last time and it was obviously wrong. Also it's just like... wrong.

---
+++ ROMniscience/Handlers/Stubs/Laser200.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * The MIT License
+ *
+ * Copyright 2018 Megan Leet (Zowayix).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMniscience.Handlers.Stubs {
+	class Laser200 : StubHandler {
+		//One of those things where there's so many different versions under different names in different countries (Dick Smith VZ200 over here, for example) that it becomes hard to tell what the original name is. I think this is the right one, though
+		public override IDictionary<string, string> filetypeMap => new Dictionary<string, string> {
+			{"vz", "VTech Laser 200 snapshot"},
+			{"cas", "VTech Laser 200 tape"},
+		};
+
+		public override string name => "VTech Laser 200";
+	}
+}
