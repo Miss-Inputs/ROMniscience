2cb2c737e640462057850632ac536499a26c0d38
Megan Leet <megan.leet@iinet.net.au>
2018-07-06 23:42:02 +1000

    Add Apple IIgs stub handler.

---
+++ ROMniscience/Handlers/Stubs/AppleIIGS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMniscience.Handlers.Stubs {
+	class AppleIIGS : StubHandler {
+		public override IDictionary<string, string> filetypeMap => new Dictionary<string, string>{
+			{"2mg", "Apple IIgs disk image"},
+		};
+
+		public override string name => "Apple IIgs";
+	}
+}
