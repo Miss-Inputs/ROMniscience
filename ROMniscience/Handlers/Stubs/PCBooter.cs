d2d4d14b10d1466248379ea2a207a6b15eedafb2
Megan Leet <megan.leet@iinet.net.au>
2018-07-13 14:09:14 +1000

    Add PC booter stub handler
    Hmm....

---
+++ ROMniscience/Handlers/Stubs/PCBooter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMniscience.Handlers.Stubs {
+	class PCBooter : StubHandler {
+		public override IDictionary<string, string> filetypeMap => new Dictionary<string, string> {
+			{"img", "IBM PC boot disk image"},
+		};
+
+		public override string name => "IBM PC Booter";
+	}
+}
