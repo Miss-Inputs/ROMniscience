c2c1701d06f9b2980ca51e8c7cbdf7f88946c95f
Megan Leet <megan.leet@iinet.net.au>
2018-07-13 13:44:32 +1000

    Add Microvision stub handler.

---
+++ ROMniscience/Handlers/Stubs/Microvision.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMniscience.Handlers.Stubs {
+	class Microvision : StubHandler {
+		public override IDictionary<string, string> filetypeMap => new Dictionary<string, string> {
+			{"bin", "Microvision ROM"},
+		};
+
+		public override string name => "Microvision";
+	}
+}
